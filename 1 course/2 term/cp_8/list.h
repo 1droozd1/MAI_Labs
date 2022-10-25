175b0a699ea5003088775a5ebd42db5bc4e08f8c
1droozd1 <dubr0vin-dk@mail.ru>
2022-10-26 02:12:44 +0300

    smth

---
+++ 1 course/2 term/cp_8/list.h
@@ -0,0 +1,30 @@
+#ifndef list_h
+#define list_h
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdbool.h"
+#include "string.h"
+
+#define MAXLEN 256
+
+typedef struct unit {
+    char value[MAXLEN];
+    struct unit *next;
+    struct unit *prev;
+}unit;
+
+typedef struct list {
+    struct unit *border;
+}list;
+
+
+bool isInt(const char*str);
+list * create_list();
+void add_list(list * l, char *value);
+void delete_list(list *l, char *value);
+int len_list(list *l);
+void expand_list(list *l, char *value, int k);
+void print_list(list *l);
+
+#endif
